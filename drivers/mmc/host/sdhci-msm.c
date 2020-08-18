MotorolaMobilityLLC/kernel-msm

mmc: host: Add variant ops for cqhci crypto
QTI implementation for block keyslot manager and
crypto vops for crypto support in CQHCI.

Change-Id: I9b64f85ca97c269a6ecd6fde2bb693745d4c43d4
Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

drivers/mmc/host/sdhci-msm.c
@@ -36,6 +36,7 @@
 #include "sdhci-msm.h"
 #include "sdhci-pltfm.h"
 #include "cqhci.h"
+#include "cqhci-crypto-qti.h"
 
 #define QOS_REMOVE_DELAY_MS	10
 #define CORE_POWER		0x0
@@ -2441,6 +2442,13 @@ static int sdhci_msm_cqe_add_host(struct sdhci_host *host,
 	msm_host->cq_host = cq_host;
 
 	dma64 = host->flags & SDHCI_USE_64_BIT_DMA;
+	/*
+	 * Set the vendor specific ops needed for ICE.
+	 * Default implementation if the ops are not set.
+	 */
+#ifdef CONFIG_MMC_CQHCI_CRYPTO_QTI
+	cqhci_crypto_qti_set_vops(cq_host);
+#endif
 
 	ret = cqhci_init(cq_host, host->mmc, dma64);
 	if (ret) {

bindal-uday/android_kernel_xiaomi_miatoll

drivers/mmc/host/sdhci-msm.c
@@ -43,6 +43,7 @@
 
 #include "sdhci-msm.h"
 #include "cmdq_hci.h"
+#include "cmdq_hci-crypto-qti.h"
 
 #define QOS_REMOVE_DELAY_MS	10
 #define CORE_POWER		0x0
@@ -4644,6 +4645,13 @@ static void sdhci_msm_cmdq_init(struct sdhci_host *host,
 	} else {
 		msm_host->mmc->caps2 |= MMC_CAP2_CMD_QUEUE;
 	}
+	/*
+	 * Set the vendor specific ops needed for ICE.
+	 * Default implementation if the ops are not set.
+	 */
+#ifdef CONFIG_MMC_CQ_HCI_CRYPTO_QTI
+	cmdq_crypto_qti_set_vops(host->cq_host);
+#endif
 }
 #else
 static void sdhci_msm_cmdq_init(struct sdhci_host *host,